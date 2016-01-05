afa1d03529067605ab2f9c05526bab1ac849d8da
Aryan <ali.mbstu.ict@gmail.com>
2016-01-06 00:47:47 +0600

    App Constant Update

--- AndroidCrashLogger/app/src/main/java/com/vaiuu/androidcrashlogger/AppConstant.java
+++ AndroidCrashLogger/app/src/main/java/com/vaiuu/androidcrashlogger/AppConstant.java
@@ -4,9 +4,9 @@ public class AppConstant {
 
 	public static final String LOG_DIR = "/www/CrashLog/logs/";
 	public static String CRASH_MSG = "Message sent to Developer";
-	public static String EMAIL_ADDRESS = "mothership.auger@gmail.com";
-	public static String EMAILPASSWORD = "scotiahouse";
-	public static String[]  RECEIVER_EMAIL_ADDRESS ={ "ali.mbstu.ict@gmail.com" };
+	public static String EMAIL_ADDRESS = " sender email address here";
+	public static String EMAILPASSWORD = " sender email password here";
+	public static String[]  RECEIVER_EMAIL_ADDRESS ={ "receiver email" };
 
 	public static String EMAIL_SUBJECT = "Crah Log";
 	public static String EMAIL_BODY = "This mail is come from user with crash log attached";
